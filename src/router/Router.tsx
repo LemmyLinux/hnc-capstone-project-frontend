e39270d8c666dbb28c29a8080c87ac8d3845aae9
Alba Rocamora <albamaria.rocamora@gmail.com>
2024-02-29 17:58:44 +0100

    Added router component

---
+++ src/router/Router.tsx
@@ -0,0 +1,12 @@
+import * as React from "react";
+import { Routes, Route } from 'react-router-dom';
+
+function Router() {
+  return (
+    <Routes>
+      <Route path="/" element={null} />
+    </Routes>
+  )
+}
+
+export default Router;
