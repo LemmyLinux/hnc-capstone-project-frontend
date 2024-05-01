6c194914ec1464f69e1530a42507a8125ccaeef0
Alba Rocamora <albamaria.rocamora@gmail.com>
2024-05-01 21:11:36 +0200

    Solved date timezone issue

--- src/common/dates.tsx
+++ src/common/dates.tsx
@@ -59,7 +59,7 @@ export function getFormatedDate(date: Date) {
  * @returns string formateado
  */
 export function getFormmatedTime(date: Date) {
-    return date.toLocaleString('en', {timeZone: 'Europe/Madrid'}).substring(10, 15);
+    return date.toLocaleString('en', {timeZone: 'Europe/Madrid'}).substring(9, 14   );
 }
 
 /**
